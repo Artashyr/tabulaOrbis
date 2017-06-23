dd8c179ac3b84d1842227caa9d6bc573d756c287
Joshua Hartley <jhartley@selectquotesenior.com>
2017-06-22 21:47:12 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './images/logo.gif';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+            <div className="App-headerImage">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p className="App-intro">
+                    Welcome to my world
+                </p>
+            </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
